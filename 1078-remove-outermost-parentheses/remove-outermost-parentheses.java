dbf294e891ec4bc47addde151916af0c2f2a99df
Harmeet Singh <hsinghm098@gmail.com>
2025-02-25 22:09:50 +0530

    Time: 2 ms (99.02%) | Memory: 42.4 MB (53.31%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,22 +1,39 @@
+//ChatGpt
+// class Solution { 
+//     public String removeOuterParentheses(String s) {
+//         StringBuilder s1 = new StringBuilder();
+//         int count = 0;  // Counter to track the depth of parentheses
+
+//         for (char ch : s.toCharArray()) {
+//             if (ch == '(') {
+//                 if (count > 0) s1.append(ch);  // Add only if it's not an outer '('
+//                 count++;
+//             } else { // ch == ')'
+//                 count--;
+//                 if (count > 0) s1.append(ch);  // Add only if it's not an outer ')'
+//             }
+//         }
+//         return s1.toString();
+//     }
+// }
+
 class Solution {
     public String removeOuterParentheses(String s) {
     char[] arr=s.toCharArray();
     int n=arr.length;
     int c=0;
-    int n1=0;
     StringBuilder s1=new StringBuilder();
     for(int i=0;i<n;i++)
     {
       if(arr[i]=='(' )
       {
-        c++;
-        if(c>1)
+        if(c>0)
         s1.append("(");
+        c++;
       }
       else if(arr[i]==')')
       {
         c--;
-        n1++;
         if(c>0)
         s1.append(")");
       }
