3221e58e7948b518ae6c5cd636d62126bd2e79f5
Harmeet Singh <hsinghm098@gmail.com>
2025-03-01 20:22:53 +0530

    Time: 6 ms (97.94%) | Memory: 48.1 MB (44.91%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,33 +1,33 @@
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-      List<List<String>> a =new ArrayList<>();
-      HashMap<String,List<String>> h= new HashMap<>();
-      for(int i=0;i<strs.length;i++)
-      {
-       char[] arr=strs[i].toCharArray();
-       Arrays.sort(arr);
-       String s1=new String(arr);
-       h.computeIfAbsent(s1,k -> new ArrayList<>()).add(strs[i]);
-      }
-      for(List<String> l :h.values())
-      {
-        a.add(l);
-      }
-    return a;    
-    }
-}
-
-
 // class Solution {
 //     public List<List<String>> groupAnagrams(String[] strs) {
-//        HashMap<String,List<String>> h=new HashMap<>();
-//        for(int i=0;i<strs.length;i++)
-//        {
-//         char[] arr=strs[i].toCharArray();
-//         Arrays.sort(arr);
-//         String s1 = new String(arr);
-//         h.computeIfAbsent(s1, k -> new ArrayList<>()).add(strs[i]);
-//        }
-//       return new ArrayList<>(h.values());    
+//       List<List<String>> a =new ArrayList<>();
+//       HashMap<String,List<String>> h= new HashMap<>();
+//       for(int i=0;i<strs.length;i++)
+//       {
+//        char[] arr=strs[i].toCharArray();
+//        Arrays.sort(arr);
+//        String s1=new String(arr);
+//        h.computeIfAbsent(s1,k -> new ArrayList<>()).add(strs[i]);
+//       }
+//       for(List<String> l :h.values())
+//       {
+//         a.add(l);
+//       }
+//     return a;    
 //     }
 // }
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       HashMap<String,List<String>> h=new HashMap<>();
+       for(int i=0;i<strs.length;i++)
+       {
+        char[] arr=strs[i].toCharArray();
+        Arrays.sort(arr);
+        String s1 = new String(arr);
+        h.computeIfAbsent(s1, k -> new ArrayList<>()).add(strs[i]);
+       }
+      return new ArrayList<>(h.values());    
+    }
+}
