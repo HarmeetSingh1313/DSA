0c43a214a933fd7a593ae745234b31e272d7c521
Harmeet Singh <hsinghm098@gmail.com>
2025-03-01 16:13:05 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (80.05%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -7,7 +7,7 @@ class Solution {
     {
         i++;
     }
-    if((i<n) && (s.charAt(i)=='+' || s.charAt(i)=='-'))
+    if((i<n) && (s.charAt(i)=='+' || s.charAt(i)=='-'))  //mistake due to () as i not add this () which leads to precedence issue
     {
      if(s.charAt(i)=='-')
       sign=-1;
@@ -16,11 +16,11 @@ class Solution {
     while(i<n && Character.isDigit(s.charAt(i)))
     {
       ans=ans*10+(s.charAt(i)-'0');
-      if(ans>Integer.MAX_VALUE)
+      if(ans>Integer.MAX_VALUE)   //to check overflow 
        {
         return (sign==1)?Integer.MAX_VALUE:Integer.MIN_VALUE;
        }
-      i++;
+      i++;                 // earlier mistake:- not incrementing this which leads to TLE(time limit exceed)
     }
     return (int)(ans*sign);  
     }
