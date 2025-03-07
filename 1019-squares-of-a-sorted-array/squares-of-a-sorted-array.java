42428210d76c80938771ad803106ebe100fbc36b
Harmeet Singh <hsinghm098@gmail.com>
2025-03-07 21:04:45 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (66.56%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,12 +1,38 @@
+//Brute force
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         int n=nums.length;
+//         int[] arr=new int[n];
+//         for(int i=0;i<n;i++)
+//         {
+//             arr[i]=nums[i]*nums[i];
+//         }
+//         Arrays.sort(arr);
+//         return arr;
+//     }
+// }
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n=nums.length;
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++)
+        int i=0;
+        int j=nums.length-1;
+        int c=j;
+        int[] arr=new int[c+1];
+        while(i<=j)
         {
-            arr[i]=nums[i]*nums[i];
+            if((nums[i]*nums[i]) < (nums[j]*nums[j]))
+            {
+                arr[c]=(nums[j]*nums[j]);
+                c--;
+                j--;
+            }
+            else //if((nums[i]*nums[i]) > (nums[j]*nums[j])) not required because whem num[i]=num[j] then it will not run
+            {
+                arr[c]=(nums[i]*nums[i]);
+                c--;
+                i++;
+            }
         }
-        Arrays.sort(arr);
-        return arr;
+    return arr;    
     }
 }
