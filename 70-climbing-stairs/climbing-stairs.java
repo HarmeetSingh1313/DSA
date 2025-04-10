11233bcd1b3646af172ae20080c8109010f586d2
Harmeet Singh <hsinghm098@gmail.com>
2025-04-10 20:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (50.32%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -19,18 +19,36 @@
 // }
 
 // my solution(DP)
+// class Solution {    
+//     public int climbStairs(int n) {
+//     int[] dp=new int [n+1];
+//     Arrays.fill(dp,-1);
+//     return fxn(n,dp);
+//     }
+//     public int fxn(int n,int[] dp)
+//     {
+//        if(n==0 || n==1 || n==2)
+//         return n;
+//        if(dp[n]!=-1)
+//         return dp[n];
+//        return dp[n]=fxn(n-1,dp)+fxn(n-2,dp);   
+//     }
+// }
+
+//one more chat gpt solution ( more optimized )
 class Solution {    
     public int climbStairs(int n) {
-    int[] dp=new int [n+1];
-    Arrays.fill(dp,-1);
-    return fxn(n,dp);
-    }
-    public int fxn(int n,int[] dp)
+    if(n<=2)
+     return n;    
+    int first=1;
+    int second=2;
+    int current=0;
+    for(int i=3;i<n+1;i++)
     {
-       if(n==0 || n==1 || n==2)
-        return n;
-       if(dp[n]!=-1)
-        return dp[n];
-       return dp[n]=fxn(n-1,dp)+fxn(n-2,dp);   
+      current=first+second;
+      first=second;
+      second=current;
     }
+    return current;
+}
 }
