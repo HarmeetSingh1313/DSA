26c1bfe6bd3f12cfde53231f860f7de41e000cfe
Harmeet Singh <hsinghm098@gmail.com>
2025-09-14 17:38:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (46.07%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -8,28 +8,55 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-       ListNode dummy=new ListNode(-1);
-       ListNode c=dummy;
-       ListNode p1=list1;
-       ListNode p2=list2;
-       while(p1!=null && p2!=null)
-       {
-        if(p1.val <p2.val)
-        {
-            c.next=p1;
-            p1=p1.next;
-        }
-        else
-        {
-            c.next=p2;
-            p2=p2.next;
-        }
-        c=c.next;
-       }
+
+
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        ListNode dummy=new ListNode(-1);
+//        ListNode c=dummy;
+//        ListNode p1=list1;
+//        ListNode p2=list2;
+//        while(p1!=null && p2!=null)
+//        {
+//         if(p1.val <p2.val)
+//         {
+//             c.next=p1;
+//             p1=p1.next;
+//         }
+//         else
+//         {
+//             c.next=p2;
+//             p2=p2.next;
+//         }
+//         c=c.next;
+//        }
        
-    c.next=(p1==null)?p2:p1;
-    return dummy.next;   
-    }
-}
+//     c.next=(p1==null)?p2:p1;
+//     return dummy.next;   
+//     }
+// }
+
+
+//       Recursive Solution
+
+class Solution {
+     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+       if(list1 == null)
+        return list2;
+
+       if(list2 == null)
+        return list1; 
+
+       if(list1.val < list2.val)
+       {
+         list1.next=mergeTwoLists(list1.next,list2);
+         return list1;
+       }
+       else
+       {
+        list2.next=mergeTwoLists(list1,list2.next);
+        return list2;
+       } 
+     }
+} 
+
